323fe0b161827ae86d6d6107abd1df698480b698
Evan Sharp <ego@evansharp.ca>
2018-03-31 15:44:43 -0700

    update database init slq to include new resistance var

--- trailsendmonitor.sql
+++ trailsendmonitor.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 28, 2018 at 08:09 AM
--- Server version: 5.7.21-0ubuntu0.16.04.1
--- PHP Version: 7.0.28-0ubuntu0.16.04.1
+-- Host: localhost:3306
+-- Generation Time: Mar 31, 2018 at 03:44 PM
+-- Server version: 10.1.23-MariaDB-9+deb9u1
+-- PHP Version: 7.0.27-0+deb9u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `trailsendmonitor`
@@ -29,13 +23,17 @@ USE `trailsendmonitor`;
 --
 
 DROP TABLE IF EXISTS `data`;
-CREATE TABLE `data` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `data` (
+  `id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `stream-id` int(11) NOT NULL,
   `timestamp` datetime NOT NULL,
   `label` varchar(6) NOT NULL,
-  `value` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `value` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `stream-id` (`stream-id`),
+  KEY `timestamp` (`timestamp`),
+  KEY `label` (`label`)
+) ENGINE=InnoDB AUTO_INCREMENT=161 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -44,54 +42,25 @@ CREATE TABLE `data` (
 --
 
 DROP TABLE IF EXISTS `streams`;
-CREATE TABLE `streams` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `streams` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `description` varchar(256) DEFAULT NULL,
-  `device-serial` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `device-serial` varchar(50) DEFAULT NULL,
+  `shunt_resistance` float NOT NULL DEFAULT '0',
+  `disabled` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `streams`
 --
 
-INSERT INTO `streams` (`id`, `name`, `description`, `device-serial`) VALUES
-(7, 'Test', 'A fake device to test with', 't1000');
+INSERT INTO `streams` (`id`, `name`, `description`, `device-serial`, `shunt_resistance`, `disabled`) VALUES
+(7, 'Test', 'A fake device to test with', 't1000', 0.0001, 1),
+(8, 'battery_sens', 'The sensor for the main DC Bus.', 'A5BAD', 0.1, 1);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `data`
---
-ALTER TABLE `data`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `stream-id` (`stream-id`),
-  ADD KEY `timestamp` (`timestamp`),
-  ADD KEY `label` (`label`);
-
---
--- Indexes for table `streams`
---
-ALTER TABLE `streams`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `name` (`name`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `data`
---
-ALTER TABLE `data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `streams`
---
-ALTER TABLE `streams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- Constraints for dumped tables
 --
@@ -101,7 +70,3 @@ ALTER TABLE `streams`
 --
 ALTER TABLE `data`
   ADD CONSTRAINT `streams in data` FOREIGN KEY (`stream-id`) REFERENCES `streams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
