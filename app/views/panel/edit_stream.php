c64a66d30589bcc3966ed9a33c4b3d8766393a1b
Evan Sharp <ego@evansharp.ca>
2018-04-02 08:55:12 -0700

    Another change to switch 'shunt-resistance' to 'unit'

--- app/views/panel/edit_stream.php
+++ app/views/panel/edit_stream.php
@@ -9,7 +9,7 @@
 			<fieldset class="pure-group">
 				<input type="text" length="49" size="35" id="new_stream_name" name="new_stream_name" placeholder="Device Name" value="<?php echo $stream['name'];?>">
 				<input type="text" length="49" size="35" id="new_stream_serial" name="new_stream_serial" placeholder="Serial Number" value="<?php echo $stream['device-serial'];?>">
-				<input type="number" length="4" size="4" id="new_stream_resistance" name="new_stream_resistance" step="0.0001" placeholder="Shunt Resistence (ohms)" value="<?php echo $stream['shunt_resistance'];?>">
+				<input type="text" length="4" size="4" id="new_stream_unit" name="new_stream_unit" placeholder="Stream Unit" value="<?php echo $stream['unit'];?>">
 			</fieldset>
 			<fieldset class="pure-group">
 				<textarea id="new_stream_desc" name="new_stream_desc" cols="40" rows="4" placeholder="Description"><?php echo $stream['description'];?></textarea>
