db01c2c3d916483aebd2c749a3ef5947f3b6c612
Evan Sharp <ego@evansharp.ca>
2018-05-19 17:20:39 -0700

    settings view changes

---
+++ app/views/panel/view_stats.php
@@ -0,0 +1,36 @@
+<div class="pure-g">
+	<div class="pure-u-1-3"></div>
+	<div class="pure-u-1-3"></div>
+	<div class="pure-u-1-3"><h2>Statistics</h2></div>
+</div>
+
+<div class="pure-g">
+	<div class="pure-u-1-1"><h3>Generation</h3></div>
+	<div class="pure-u-1-3"></div>
+	<div class="pure-u-2-3">
+		<?php
+			// Solar A production in Ah:
+			//	- Today (since midnight)
+			//	- This week
+			//	- This month
+			//	- Since last reset ("all time")
+
+			// Solar B production in Ah:
+			//	- Today (since midnight)
+			//	- This week
+			//	- This month
+			//	- Since last reset ("all time")
+
+			// Hydro production in Ah:
+			//	- Today (since midnight)
+			//	- This week
+			//	- This month
+			//	- Since last reset ("all time")
+
+			
+		?>
+		<table>
+		</table>
+
+	</div>
+</div>
